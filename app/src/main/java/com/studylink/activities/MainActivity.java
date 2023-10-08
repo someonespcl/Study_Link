c990857f06259da559518783d8cb7a9328fa80e8
someonespcl <madhavpoola4@gmail.com>
2023-10-08 13:54:02 +0530

    first commit

---
+++ app/src/main/java/com/studylink/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package com.studylink.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.studylink.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Inflate and get instance of binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // set content view to binding's root
+        setContentView(binding.getRoot());
+    }
+}
---
+++ app/src/main/java/com/studylink/activities/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.studylink.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+import com.studylink.databinding.LayoutSplashScreenBinding;
+import com.studylink.R;
+
+public class SplashScreen extends AppCompatActivity {
+    
+    private LayoutSplashScreenBinding binding;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = LayoutSplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //start the main activity
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, 2000); //(2 seconds) delay
+    }
+}
